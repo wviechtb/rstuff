e04b8eb505eaf268ebd34fabc2e09a0d3dd0c063
Wolfgang Viechtbauer <wvb@wvbauer.com>
2019-05-24 17:43:28 +0200

    Updated library.r so the function is put into its own environment.

--- library.r
+++ library.r
@@ -1,4 +1,4 @@
-# An 'improved' library() command that, if necessary, first installs a
+# An 'improved' library() function that, if necessary, first installs a
 # package before trying to load it. It can also take multiple package names
 # at once (and requires no quotes), that is, your code could start with:
 #
@@ -7,11 +7,11 @@
 # and all packages will be installed (if necessary) and then loaded.
 #
 # If you put this into your .Rprofile file, library(pkg) will call this
-# command instead of base::library(pkg). You can still use base::library(pkg)
-# if you want to make use of the 'base' package library() command.
+# function instead of base::library(pkg). You can still use base::library(pkg)
+# if you want to make use of the 'base' package library() function.
 #
-# Note that if you share code with others that uses this command, you should
-# stick to:
+# Note that if you share code with others that makes use of this function,
+# you should stick to:
 #
 # library(pkg1)
 # library(pkg2)
@@ -19,14 +19,18 @@
 # ...
 #
 # at the beginning of your code so it remains runnable for those who do not
-# have this command (of course, they would have to manually install the
+# have this function (of course, they would have to manually install the
 # packages first if they are not already installed).
 #
-# Note that if you run rm(list=ls()) to clear your workspace, this will also
-# remove the function below from your workspace.
+# The following line creates an environment into which we will store the
+# library() function. By prefixing the environment with a dot, it is not
+# directly visible with ls() and hence running rm(list=ls()) to clear your
+# workspace will not delete this environment (and hence not the function).
 
-library <- function(...) {
-   ### install.packages() gives a warning if it fails; upgrade warnings to errors so try() works as intended
+.rstuff <- new.env()
+
+.rstuff$library <- function(...) {
+   # install.packages() gives a warning if it fails; upgrade warnings to errors so try() works as intended
    wopt <- options(warn=2)
    on.exit(options(warn=wopt$warn)) # on exit, set 'warn' back to previous setting
    pkgs <- as.character(substitute(list(...)))[-1L] # -1L to remove 'list'
@@ -60,3 +64,9 @@ library <- function(...) {
       cat("\n")
    }
 }
+
+# Now we attach the .rstuff environment so that calling library() will call
+# the function above and not base::library(). With 'warn.conflicts=FALSE', we
+# avoid the note that the base package library() function has been masked.
+
+attach(.rstuff, warn.conflicts=FALSE)
